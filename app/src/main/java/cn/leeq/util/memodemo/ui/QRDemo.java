ea26d50b6ab35a3e4dd6214c21a544b3baf2edf1
EnivLee <lqiang0219@gmail.com>
2016-07-12 13:23:16 +0800

    添加ZXing模块 包括扫描和生成二维码

---
+++ app/src/main/java/cn/leeq/util/memodemo/ui/QRDemo.java
@@ -0,0 +1,13 @@
+package cn.leeq.util.memodemo.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QRDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrdemo);
+    }
+}
