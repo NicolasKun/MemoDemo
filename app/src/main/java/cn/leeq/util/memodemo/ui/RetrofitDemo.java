6149d398ebf7fd00419670acaf4a21480f2be3d3
EnivLee <lqiang0219@gmail.com>
2016-08-23 09:00:11 +0800

    Simple GET of Retrofit~

--- app/src/main/java/cn/leeq/util/memodemo/ui/RetrofitDemo.java
+++ app/src/main/java/cn/leeq/util/memodemo/ui/RetrofitDemo.java
@@ -2,8 +2,8 @@ package cn.leeq.util.memodemo.ui;
 
 import android.os.Bundle;
 import android.util.Log;
-
-import org.xutils.db.converter.ColumnConverterFactory;
+import android.view.View;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.util.List;
@@ -13,7 +13,6 @@ import cn.leeq.util.memodemo.Yu;
 import cn.leeq.util.memodemo.bean.Status;
 import retrofit2.Call;
 import retrofit2.Callback;
-import retrofit2.Converter;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -28,21 +27,41 @@ public class RetrofitDemo extends BaseActivity {
 
     public static final String API_URL = "https://api.github.com";
     private Retrofit retrofit;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrofit_demo);
+        tvResult = (TextView) findViewById(R.id.rd_tv_result);
         retrofit = new Retrofit.Builder()
                 .baseUrl(API_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+    }
 
-        try {
-            loadData();
-        } catch (IOException e) {
-            e.printStackTrace();
+    private String test(String all) {
+        if (all.equals("") && all.length() <= 0) {
+            throw new NullPointerException("Do not found everything in \'all\'");
         }
+        if (all.split(" ").length <= 0) {
+            throw new IllegalArgumentException("Do not found \' \' in \'all\' ,please check");
+        }
+        String[] split = all.split(" ");
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < split.length; i++) {
+            String a = split[i];
+            if (a.length() == 1) {
+                sb.append(a + " ");
+            } else {
+                String aa = a.substring(0, 1) + "ay";  //截取头字母
+                String bb = a.substring(1, a.length()); //剩下的字符串
+                String current = bb + aa;
+                sb.append(current + " ");
+            }
+        }
+        System.out.println("结果\n" + sb.toString());
+        return sb.toString();
     }
 
     private void loadData() throws IOException {
@@ -55,7 +74,10 @@ public class RetrofitDemo extends BaseActivity {
             public void onResponse(Call<List<Status>> call, Response<List<Status>> response) {
                 List<Status> body = response.body();
                 for (Status status : body) {
-                    Log.e("retrofit", "结果码 " + response.code() + "\n结果 " + status.getContributions());
+                    String s = "结果    " + status.getContributions() +
+                            "\n登陆者 " + status.getLogin() + "\n登陆ID " + status.getId();
+                    tvResult.setText(s);
+                    Log.e("retrofit", "结果码 " + response.code() + "\n" + s);
                 }
             }
 
@@ -64,5 +86,13 @@ public class RetrofitDemo extends BaseActivity {
                 Log.e("retrofit", t.getMessage());
             }
         });
+    }
+
+    public void getResult(View view) {
+        try {
+            loadData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
